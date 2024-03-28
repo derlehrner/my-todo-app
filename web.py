6041460bb20f456aa074ed4c2896fbae26b1934c
derlehrner <miaphd2019@gmail.com>
2024-03-28 16:02:30 -0600

    Added bar graph and chart emojis

--- web.py
+++ web.py
@@ -12,7 +12,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app exists to help you increase your productivity.📊")
+st.write("This app exists to help you increase your 📈productivity.📊")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
